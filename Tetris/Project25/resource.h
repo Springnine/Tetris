4f2a5d92d5c3bc0b17e02f1c731b662ed921a234
Springnine <82144761+Springnine@users.noreply.github.com>
2021-12-07 19:51:39 +0900

    Add files via upload
    윈도우즈 API 정복을 보고 만든 테트리스입니다.

---
+++ Tetris/Project25/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Project25.rc에서 사용되고 있습니다.
+//
+#define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     104
+#define IDB_BITMAP3                     105
+#define IDB_BITMAP4                     106
+#define IDB_BITMAP5                     107
+#define IDB_BITMAP6                     108
+#define IDB_BITMAP7                     109
+#define IDB_BITMAP8                     110
+#define IDB_BITMAP9                     111
+#define IDB_BITMAP10                    112
+#define IDB_BITMAP11                    113
+#define IDM_GAME_START                  40001
+#define IDM_GAME_PAUSE                  40002
+#define IDM_GAME_EXIT                   40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
